b973ce55ea41a29663becee1e336d5a3f5c4ba8c
krotxe <krotxe@gmail.com>
2015-05-11 23:13:18 +0200

    Cambio menor

--- src/Pokemon/VentanaPokedex.java
+++ src/Pokemon/VentanaPokedex.java
@@ -269,6 +269,7 @@ public class VentanaPokedex extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
+        setResizable(false);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setForeground(new java.awt.Color(255, 255, 255));
